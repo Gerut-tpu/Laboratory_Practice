6882e2dc1b9b2000ece8ca205574f793f04283e6
Gerut-tpu <tymbaumba2015@gmail.com>
2025-12-14 18:38:15 +0700

    RCC and GPIO

---
+++ Core/Inc/init.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "../../CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h"
+#include "../../CMSIS/Devices/STM32F4xx/Inc/STM32F429xx/stm32f429xx.h"
+void RCC_Init(void); //Функия настройки тактирования 
+void GPIO_Init(void); //Функция настройки портов GPIO
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,15 @@
+#include "init.h"
 int main(void)
 {
-    while(1){}
+    RCC_Init();
+    GPIO_Init();
+    while(1)
+    {
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+        SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS0);
+        SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS1);
+        SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS2);
+    }
 }
